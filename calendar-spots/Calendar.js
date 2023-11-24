d4480a70dbaf95823ee7f73c4669d865cf4ac02d
javier <javier.chavarino.1991@gmail.com>
2023-11-24 19:47:45 +0100

    removing comments

--- calendar-spots/Calendar.js
+++ calendar-spots/Calendar.js
@@ -2,12 +2,7 @@ const moment = require('moment')
 const { CalendarFileReader } = require('./calendar-reader')
 const { CalendarLogic } = require('./calendar-logic')
 
-// crear clase calendar 
-// inyeccion de dependencias del reader de archivos crear adaptador (read) 
-// dependencias del moment tambien puede ser buena idea  directamente porque es grande la impl
-//calendar :
-// calendarId, date, duration, rawdatacalendar
-//dateIso
+
 const calendarReader = new CalendarFileReader()
 
 
